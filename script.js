158617251b7a35c6a7aa06ae52233d903b49e849
kauangaldinolopes-rgb <kauan.galdino.lopes@escola.pr.gov.br>
2026-06-30 08:05:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+// Aguarda o carregamento do documento
+document.addEventListener('DOMContentLoaded', () => {
+    const btnInfo = document.getElementById('btn-info');
+    const infoExtra = document.getElementById('info-extra');
+
+    // Adiciona evento de clique para mostrar/esconder as curiosidades do MC
+    btnInfo.addEventListener('click', () => {
+        if (infoExtra.classList.contains('hidden')) {
+            infoExtra.classList.remove('hidden');
+            btnInfo.textContent = 'Ocultar Curiosidades';
+        } else {
+            infoExtra.classList.add('hidden');
+            btnInfo.textContent = 'Ver Curiosidades';
+        }
+    });
+});
